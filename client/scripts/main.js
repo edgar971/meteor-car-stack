07cfb9d98e0a8c7382d34827d8843214f411fa5a
Edgar Pino <edgarbeltran@me.com>
2015-08-16 23:50:50 -0500

    added masonry layout

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
+$(window).load(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('.datepicker').pickadate({
         selectMonths: false, // Creates a dropdown to control month
         selectYears: 60 // Creates a dropdown of 15 years to control year
     });
+
+    //start masonry plugin
+    $('.main-grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        gutter: '.grid-gutter-sizer',
+        percentPosition: true,
+
+    });
+
+
+
 });
